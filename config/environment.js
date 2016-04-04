02ebed95ce56f61d8b37d027fe8222d8008c4bba
brooke hurford and gracie lundell <gracielundell@gmail.com>
2016-04-04 08:56:22 -0700

    Add rental cost helper, show dollar signs based on cost per night

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'super-rentals',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://fiery-fire-2195.firebaseio.com/',
+    firebase: 'https://super-rental-city.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
